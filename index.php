15d58909917d6d1c6222cf49f5501dcbf7a8ac51
Anderson Ullco <anderson1995sistemas@gmail.com>
2022-01-03 21:12:19 -0500

    Se modifico el index.html

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ License: You must have a valid license purchased only from wrapbootstrap.com (li
                     <div class="page-logo">
                         <a href="#" class="page-logo-link press-scale-down d-flex align-items-center position-relative" data-toggle="modal" data-target="#modal-shortcut">
                             <img src="img/cafeterisapng.png" alt="SmartAdmin WebApp" aria-roledescription="logo">
-                            <span class="page-logo-text mr-1">CAFETERISA </span>
+                            <span class="page-logo-text mr-1">CAFETERISA  buenos dias </span>
                             <span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2"></span>
                             <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
                         </a>
